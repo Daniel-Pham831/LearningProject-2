99f82bf136f9038953a82483f1846fd855b2380d
Dan Pham <phamphuc0603@gmail.com>
2024-07-09 11:13:34 +0700

    test

---
+++ Assets/Scripts/FirstScript.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class FirstScript : MonoBehaviour
+    {
+        public float A;
+        public float B;
+        public float C;
+        
+        public void Start()
+        {
+            // Ax^2 + Bx + C = 0;
+        }
+    }
+}
+
